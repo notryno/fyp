c6bbf2e3166d3e0db1a6bdb99092ec6c039508e0
Ryan Lepcha <ryanlepcha@192.168.1.9>
2023-12-07 15:30:23 +0545

    Changed homescreen.js to HomeScreen.js

--- api/authApi.js
+++ api/authApi.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.103:8000/api/";
+const BASE_URL = "http://192.168.1.9:8000/api/";
 
 export const register = async (userData) => {
   try {
