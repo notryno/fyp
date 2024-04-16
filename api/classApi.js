929ce1c1ddb3d7ee5e2f3637fe42b1f5e15c3d16
notryno <ryanclepcha@gmail.com>
2024-04-16 22:25:53 +0545

    Refactor class-related API functions and update ClassScreen UI

--- api/classApi.js
+++ api/classApi.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 import { BASE_URL } from "./authApi";
 
-export const getTasks = async (userToken) => {
+export const getClass = async (userToken) => {
   try {
-    console.log(userToken);
     const response = await axios.get(`${BASE_URL}class/`, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching classroom:", error);
+    throw "Error fetching class";
+  }
+};
+
+export const getClassName = async (userToken) => {
+  try {
+    const response = await axios.get(`${BASE_URL}classroom/`, {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
--- screens/ClassScreen.js
+++ screens/ClassScreen.js
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList, ActivityIndicator } from "react-native";
-import { getTasks } from "../api/classApi";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+} from "react-native";
+import { getClass, getClassName } from "../api/classApi";
 import { useAuth } from "../api/authContext";
+import { Ionicons } from "@expo/vector-icons";
 
-const ClassScreen = () => {
+const ClassScreen = ({ navigation }) => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const { userToken } = useAuth();
+  const [userClass, setUserClass] = useState("");
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const usersData = await getTasks(userToken);
+        const usersData = await getClass(userToken);
+        const classNameData = await getClassName(userToken);
         setUsers(usersData);
+        const classNameDataClass = classNameData.find(
+          (element) => element.id === usersData[0].classroom
+        );
+        setUserClass(classNameDataClass.name);
         setLoading(false);
-        console.log(usersData);
       } catch (error) {
         console.error("Error fetching users:", error);
       }
@@ -27,20 +41,119 @@ const ClassScreen = () => {
     return <ActivityIndicator size="large" color="#0000ff" />;
   }
 
+  const sortedUsers = [...users].sort((a, b) =>
+    `${a.first_name} ${a.last_name}`.localeCompare(
+      `${b.first_name} ${b.last_name}`
+    )
+  );
+
+  const renderUserItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.userItem}
+      onPress={() => navigation.navigate("UserDetails", { userId: item.id })}
+    >
+      {item.profile_picture ? (
+        <Image
+          source={{ uri: item.profile_picture }}
+          style={styles.profileImage}
+        />
+      ) : (
+        <Ionicons
+          name="person-circle-outline"
+          size={60}
+          style={styles.profileImage}
+          color={"#aaa"}
+        />
+      )}
+      <View style={styles.userInfo}>
+        <Text
+          style={styles.userName}
+        >{`${item.first_name} ${item.last_name}`}</Text>
+        <Text>{item.email}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>List of Users in Class:</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Students in your Class - {userClass}</Text>
       <FlatList
-        data={users}
+        data={sortedUsers}
         keyExtractor={(item) => item.email}
-        renderItem={({ item }) => (
-          <View>
-            <Text>1 {`${item.first_name} ${item.last_name}`}</Text>
-          </View>
-        )}
+        renderItem={renderUserItem}
+        style={{ width: "100%" }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  userItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: "#f0f0f0",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 16,
+  },
+  userInfo: {
+    flex: 1,
+  },
+  userName: {
+    fontWeight: "bold",
+  },
+  button: {
+    width: "90%",
+    alignSelf: "center",
+    height: 70,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+  },
+  buttonContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  iconContainer: {
+    flex: 0.2,
+    alignItems: "center",
+  },
+  textContainer: {
+    flex: 0.6,
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  icon: {
+    borderWidth: 1,
+    borderColor: "black",
+    color: "black",
+    padding: 5,
+    borderRadius: 5,
+  },
+});
 
 export default ClassScreen;
