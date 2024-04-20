02fe04d936c8c83bbaedda698527998ab4ea2c95
notryno <ryanclepcha@gmail.com>
2024-04-20 23:39:18 +0545

    Add create course functionality and update course detail view

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import CourseDetailView, CourseListView, EnrolledCourseListView
+from .views import (
+    CourseDetailView,
+    CourseListView,
+    CreateCourseView,
+    EnrolledCourseListView,
+)
 
 urlpatterns = [
     path("courses/", CourseListView.as_view()),
     path("courses/<int:pk>/", CourseDetailView.as_view()),
     path("enrolled/", EnrolledCourseListView.as_view()),
+    path("course/", CreateCourseView.as_view()),
 ]
--- courses/views.py
+++ courses/views.py
@@ -15,6 +15,18 @@ class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        # Print the data being sent for patch
+        print("Data being sent for PATCH request:", serializer)
+
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
 
 class EnrolledCourseListView(generics.ListAPIView):
     serializer_class = CourseSerializer
@@ -31,3 +43,8 @@ class EnrolledCourseListView(generics.ListAPIView):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class CreateCourseView(generics.CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
