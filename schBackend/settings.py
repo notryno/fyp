b1350fd9f77b13aedd29fb75dedc62a186372662
notryno <ryanclepcha@gmail.com>
2024-04-22 21:15:01 +0545

    Add email template for OTP verification

--- schBackend/settings.py
+++ schBackend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -70,7 +71,7 @@ ROOT_URLCONF = "schBackend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- utilities/email_otp.py
+++ utilities/email_otp.py
@@ -3,9 +3,11 @@ import json
 
 import pyotp
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives, send_mail
 from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.utils import timezone
+from django.utils.html import strip_tags
 from django.views.decorators.csrf import csrf_exempt
 
 from authentication.models import CustomUser
@@ -27,13 +29,32 @@ def generate_and_send_otp(user):
     user.otp_created_at = timezone.now()
     user.save()
 
-    # send_mail(
-    #     "Verify Your Email with OTP",
-    #     f"Your OTP is: {otp_code}",
-    #     settings.EMAIL_HOST_USER,
-    #     [user.email],
-    #     fail_silently=False,
-    # )
+    html_message = render_to_string(
+        "email_template/verify_email.html",
+        {
+            "digit1": otp_code[0],
+            "digit2": otp_code[1],
+            "digit3": otp_code[2],
+            "digit4": otp_code[3],
+            "digit5": otp_code[4],
+            "digit6": otp_code[5],
+        },
+    )
+
+    plain_message = strip_tags(html_message)
+
+    email = EmailMultiAlternatives(
+        "Verify Your Email with OTP",
+        plain_message,  # Plain text version
+        settings.EMAIL_HOST_USER,
+        [user.email],  # List of recipient email addresses
+    )
+
+    # Attach the HTML version of the message
+    email.attach_alternative(html_message, "text/html")
+
+    # Send the email
+    email.send(fail_silently=False)
 
 
 @csrf_exempt
