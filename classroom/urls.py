c51a12b9b0742c3dfe619f92b7cc82dbb5e22439
notryno <ryanclepcha@gmail.com>
2024-04-23 18:56:13 +0545

    Add ClassroomCreateView and ClassroomDetailView to urls.py and views.py

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ClassroomListView, ClassroomViewList
+from .views import ClassroomCreateView, ClassroomListView, ClassroomViewList
 
 urlpatterns = [
     path("class/", ClassroomViewList.as_view(), name="class-list"),
     path("classroom/", ClassroomListView.as_view(), name="classroom-list"),
+    path("classroom/create/", ClassroomCreateView.as_view(), name="classroom-create"),
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -44,3 +44,36 @@ class ClassroomListView(generics.ListAPIView):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ClassroomDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ClassroomCreateView(generics.CreateAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
