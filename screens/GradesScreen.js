bf6f23d73e49877fe311a54597292c6660c8e204
notryno <ryanclepcha@gmail.com>
2024-04-23 23:55:17 +0545

    Add course information to GradesScreen

--- screens/GradesScreen.js
+++ screens/GradesScreen.js
@@ -9,25 +9,41 @@ import {
 import { DataTable } from "react-native-paper";
 import { getGrades } from "../api/gradeApi";
 import { useAuth } from "../api/authContext";
+import { getEnrolledCourses } from "../api/courseApi";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 const GradesScreen = ({ navigation }) => {
   const { userToken } = useAuth();
 
   const [grades, setGrades] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    (async () => {
+    fetchGrades();
+    fetchCourses();
+  }, [userToken]);
+
+  const fetchGrades = async () => {
+    try {
       const response = await getGrades(userToken);
       setGrades(response);
-      console.log(response);
-      setLoading(false);
-    })();
-  }, [userToken]);
+    } catch (error) {
+      console.error("Error fetching grades:", error);
+    }
+  };
+
+  const fetchCourses = async () => {
+    try {
+      const coursesData = await getEnrolledCourses(userToken);
+      setCourses(coursesData);
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    }
+  };
 
   // Function to group grades by Year and Semester
   // Function to group grades by Year and Semester
-  const groupGradesBySemester = () => {
+  const groupGradesByYearAndSemester = () => {
     // Sort grades by year and then by semester
     const sortedGrades = [...grades].sort((a, b) => {
       const yearComparison = a.course[0].year - b.course[0].year;
@@ -41,7 +57,16 @@ const GradesScreen = ({ navigation }) => {
     const groupedGrades = {};
 
     sortedGrades.forEach((grade) => {
-      const { year, semester } = grade.course[0];
+      // getCoursesInfo(grade.course[0]);
+      let { year, semester } = {};
+      try {
+        const courseInfo = getCoursesInfo(grade.course[0]);
+        year = courseInfo.year;
+        semester = courseInfo.semester;
+      } catch (error) {
+        console.error("Error getting course info:", error);
+      }
+
       const key = `${year}-${semester}`;
 
       if (!groupedGrades[key]) {
@@ -50,62 +75,100 @@ const GradesScreen = ({ navigation }) => {
 
       groupedGrades[key].push(grade);
     });
-
     return groupedGrades;
+  };
+
+  // Helper function to get year and semester information based on course ID
+  const getCoursesInfo = (courseId) => {
+    const course = courses.find((c) => c.id === courseId);
+    if (!course) {
+      return null;
+    }
+    return { year: course.year, semester: course.semester };
+  };
+
+  const getYear = (courseId) => {
+    const course = courses.find((c) => c.id === courseId);
+    if (!course) {
+      return null;
+    }
+    return course.year;
+  };
+
+  const getSemster = (courseId) => {
+    const course = courses.find((c) => c.id === courseId);
+    if (!course) {
+      return null;
+    }
+    return course.semester;
+  };
+
+  const getCode = (courseId) => {
+    const course = courses.find((c) => c.id === courseId);
+    if (!course) {
+      return null;
+    }
+    return course.code;
+  };
+
+  const getName = (courseId) => {
+    const course = courses.find((c) => c.id === courseId);
+    if (!course) {
+      return null;
+    }
+    return course.name;
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Module Results</Text>
 
-      {loading ? (
-        <ActivityIndicator />
-      ) : (
-        <ScrollView style={{ width: "100%" }}>
-          {Object.entries(groupGradesBySemester()).map(
-            ([semesterKey, semesterGrades]) => (
-              <View key={semesterKey} style={styles.card}>
-                <Text style={styles.subtitle}>
-                  {`Year: ${semesterGrades[0].course[0].year}, Semester: ${semesterGrades[0].course[0].semester}`}
-                </Text>
-                <DataTable>
-                  <DataTable.Header>
-                    <DataTable.Title style={styles.tableHeader25}>
-                      Code
-                    </DataTable.Title>
-                    <DataTable.Title style={styles.tableHeader55}>
-                      Name
-                    </DataTable.Title>
-                    <DataTable.Title style={styles.tableHeader10}>
-                      Mark
-                    </DataTable.Title>
-                    <DataTable.Title style={styles.tableHeader10}>
-                      Grade
-                    </DataTable.Title>
-                  </DataTable.Header>
+      <ScrollView style={{ width: "100%" }}>
+        {Object.entries(groupGradesByYearAndSemester()).map(
+          ([semesterKey, semesterGrades]) => (
+            <View key={semesterKey} style={styles.card}>
+              <Text style={styles.subtitle}>
+                {`Year: ${getYear(
+                  semesterGrades[0].course[0]
+                )}, Semester: ${getSemster(semesterGrades[0].course[0])}`}
+              </Text>
+              <DataTable>
+                <DataTable.Header>
+                  <DataTable.Title style={styles.tableHeader25}>
+                    Code
+                  </DataTable.Title>
+                  <DataTable.Title style={styles.tableHeader55}>
+                    Name
+                  </DataTable.Title>
+                  <DataTable.Title style={styles.tableHeader10}>
+                    Mark
+                  </DataTable.Title>
+                  <DataTable.Title style={styles.tableHeader10}>
+                    Grade
+                  </DataTable.Title>
+                </DataTable.Header>
 
-                  {semesterGrades.map((item, index) => (
-                    <DataTable.Row key={index}>
-                      <DataTable.Cell style={styles.tableCell25}>
-                        {item.course[0].code}
-                      </DataTable.Cell>
-                      <DataTable.Cell style={styles.tableCell55}>
-                        {item.course[0].name}
-                      </DataTable.Cell>
-                      <DataTable.Cell style={styles.tableCell10}>
-                        {item.score}
-                      </DataTable.Cell>
-                      <DataTable.Cell style={styles.tableCell10}>
-                        {item.grade}
-                      </DataTable.Cell>
-                    </DataTable.Row>
-                  ))}
-                </DataTable>
-              </View>
-            )
-          )}
-        </ScrollView>
-      )}
+                {semesterGrades.map((item, index) => (
+                  <DataTable.Row key={index}>
+                    <DataTable.Cell style={styles.tableCell25}>
+                      {getCode(item.course[0])}
+                    </DataTable.Cell>
+                    <DataTable.Cell style={styles.tableCell55}>
+                      {getName(item.course[0])}
+                    </DataTable.Cell>
+                    <DataTable.Cell style={styles.tableCell10}>
+                      {item.score}
+                    </DataTable.Cell>
+                    <DataTable.Cell style={styles.tableCell10}>
+                      {item.grade}
+                    </DataTable.Cell>
+                  </DataTable.Row>
+                ))}
+              </DataTable>
+            </View>
+          )
+        )}
+      </ScrollView>
     </View>
   );
 };
